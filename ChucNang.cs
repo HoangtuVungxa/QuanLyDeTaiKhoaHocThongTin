8a8c460a8604f8380835269385eba1f14597927a
Tuan <phuonghoangit2614@gmail.com>
2024-01-03 09:50:59 +0700

    #12 - Linh add main login doimatkhau

---
+++ ChucNang.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace QuanLyDeTai
+{
+    internal class ChucNang
+    {
+        public static SqlConnection Conn;  //Khai báo đối tượng kết nối
+        public static void KetNoi()
+        {
+            string source = "server = HPK01\\MAYCHU;" + "uid = tk1; pwd = 1;" + "database = QuanLyDeTaiWEB";
+            Conn = new SqlConnection(source);   //Khởi tạo đối tượng            
+            Conn.Open();                  //Mở kết nối
+            //Kiểm tra kết nối
+            if (Conn.State == ConnectionState.Open)
+                MessageBox.Show("Kết nối đến SQL server thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Không thể kết nối với SQL server!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+        public static void NgatKetNoi()
+        {
+            try
+            {
+                if (Conn.State == ConnectionState.Open)
+                {
+                    Conn.Close();       //Đóng kết nối
+                    Conn.Dispose();     //Giải phóng tài nguyên
+                    Conn = null;
+                    Application.Exit();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Lỗi kết nối!");
+            }
+        }
+        //Lấy dữ liệu vào bảng
+        public static DataTable TaiDuLieuVaoBang(string sql)
+        {
+
+            SqlDataAdapter dapt = new SqlDataAdapter(sql, Conn); //Định nghĩa đối tượng thuộc lớp SqlDataAdapter
+            //Khai báo đối tượng table thuộc lớp DataTable
+            DataTable table = new DataTable();
+            dapt.Fill(table);
+            return table;
+        }
+
+        //Hàm kiểm tra mã có trùng nhau ko
+        public static bool KiemTraTrung(string sql)
+        {
+            SqlDataAdapter dapt = new SqlDataAdapter(sql, Conn);
+            DataTable table = new DataTable();
+            dapt.Fill(table);
+            if (table.Rows.Count > 0)
+                return true;
+            else return false;
+        }
+
+        //Hàm thực hiện câu lệnh SQL
+        public static void ChayCauLenhSQL(string sql)
+        {
+            SqlCommand cmd; //Đối tượng thuộc lớp SqlCommand
+            cmd = new SqlCommand();
+            cmd.Connection = Conn; //Gán kết nối
+            cmd.CommandText = sql; //Gán lệnh SQL
+            try
+            {
+                cmd.ExecuteNonQuery(); //Thực hiện câu lệnh SQL
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();//Giải phóng bộ nhớ
+            cmd = null;
+        }
+
+        //Lấy dữ liệu từ câu lệnh SQL đổ vào ComboBox
+        public static void DuaDuLieuVaoComboBox(string sql, ComboBox cbo, string ma)
+        {
+            SqlDataAdapter dapt = new SqlDataAdapter(sql, Conn);
+            DataTable table = new DataTable();
+            dapt.Fill(table);
+            cbo.DataSource = table;
+            cbo.ValueMember = ma; //Trường giá trị            
+        }
+
+        //Lấy dữ liệu từ câu lệnh SQL đổ vào ComboBox và đổi tên mã
+        public static void DuaDuLieuVaoComboBox(string sql, ComboBox cbo, string ma, string ten)
+        {
+            SqlDataAdapter dapt = new SqlDataAdapter(sql, Conn);
+            DataTable table = new DataTable();
+            dapt.Fill(table);
+            cbo.DataSource = table;
+            cbo.ValueMember = ma; //Đưa ra các giá trị trong combobox
+            cbo.DisplayMember = ten; //Thay thế các giá trị bằng tên "ten"
+        }
+
+        //Lấy dữ liệu từ câu lệnh SQL
+        public static string LayDuLieuTuSQL(string sql)
+        {
+            string ma = "";
+            SqlCommand cmd = new SqlCommand(sql, Conn);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+                ma = reader.GetValue(0).ToString();
+            //MessageBox.Show(ma);
+            reader.Close();
+            return ma;
+        }
+
+        //Lấy dữ liệu từ 1 cột và lưu vào list
+        public static List<string> LayDuLieuCotDuaVaoList(string sql, string colName)
+        {
+            // Create a list to store the values from the column
+            List<string> columnValues = new List<string>();
+            string columnName = colName;
+            using (SqlCommand command = new SqlCommand(sql, Conn))
+            {
+                // Execute the query and obtain a SqlDataReader
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // Iterate through the rows returned by the query
+                    while (reader.Read())
+                    {
+                        // Extract the value from the specified column and add it to the list
+                        string value = reader.GetString(reader.GetOrdinal(columnName)).ToString().Trim();
+                        columnValues.Add(value);
+                    }
+                }
+            }
+            return columnValues;
+        }
+        
+    }
+}
---
+++ Login.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace QuanLyDeTai
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("UTM Helve", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PHẦN MỀM QUẢN LÝ NHIỆM VỤ NGHIÊN CỨU KHOA HỌC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("UTM Bienvenue", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label2.Location = new System.Drawing.Point(66, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("UTM Bienvenue", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label3.Location = new System.Drawing.Point(66, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật khẩu";
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(193, 99);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(189, 20);
+            this.txtTenDangNhap.TabIndex = 3;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(193, 133);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(189, 20);
+            this.txtMatKhau.TabIndex = 4;
+            this.txtMatKhau.UseSystemPasswordChar = true;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Font = new System.Drawing.Font("UTM Aurora", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(193, 214);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(189, 28);
+            this.btnDangKy.TabIndex = 5;
+            this.btnDangKy.Text = "Chưa có tài khoản? Click để đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("UTM Alexander", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(307, 169);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 30);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("UTM Alexander", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(193, 169);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(91, 30);
+            this.btnDangNhap.TabIndex = 7;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(487, 340);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TRANG ĐĂNG NHẬP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
+
